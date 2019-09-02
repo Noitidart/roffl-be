110400afeeba8f308b96930c31055fee78e77eb5
Noitidart <noitidart@gmail.com>
2019-09-02 15:18:55 -0700

    testing

---
+++ config/routes.js
@@ -0,0 +1,92 @@
+/**
+ * Route Mappings
+ * (sails.config.routes)
+ *
+ * Your routes tell Sails what to do each time it receives a request.
+ *
+ * For more information on configuring custom routes, check out:
+ * https://sailsjs.com/anatomy/config/routes-js
+ */
+
+let retryCnt = 0;
+
+module.exports.routes = {
+
+  //  ╦ ╦╔═╗╔╗ ╔═╗╔═╗╔═╗╔═╗╔═╗
+  //  ║║║║╣ ╠╩╗╠═╝╠═╣║ ╦║╣ ╚═╗
+  //  ╚╩╝╚═╝╚═╝╩  ╩ ╩╚═╝╚═╝╚═╝
+  'GET /':                   { action: 'view-homepage-or-redirect' },
+  'GET /welcome/:unused?':   { action: 'dashboard/view-welcome' },
+
+  'GET /faq':                { action:   'view-faq' },
+  'GET /legal/terms':        { action:   'legal/view-terms' },
+  'GET /legal/privacy':      { action:   'legal/view-privacy' },
+  'GET /contact':            { action:   'view-contact' },
+
+  'GET /signup':             { action: 'entrance/view-signup' },
+  'GET /email/confirm':      { action: 'entrance/confirm-email' },
+  'GET /email/confirmed':    { action: 'entrance/view-confirmed-email' },
+
+  'GET /login':              { action: 'entrance/view-login' },
+  'GET /password/forgot':    { action: 'entrance/view-forgot-password' },
+  'GET /password/new':       { action: 'entrance/view-new-password' },
+
+  'GET /account':            { action: 'account/view-account-overview' },
+  'GET /account/password':   { action: 'account/view-edit-password' },
+  'GET /account/profile':    { action: 'account/view-edit-profile' },
+
+
+  //  ╔╦╗╦╔═╗╔═╗  ╦═╗╔═╗╔╦╗╦╦═╗╔═╗╔═╗╔╦╗╔═╗   ┬   ╔╦╗╔═╗╦ ╦╔╗╔╦  ╔═╗╔═╗╔╦╗╔═╗
+  //  ║║║║╚═╗║    ╠╦╝║╣  ║║║╠╦╝║╣ ║   ║ ╚═╗  ┌┼─   ║║║ ║║║║║║║║  ║ ║╠═╣ ║║╚═╗
+  //  ╩ ╩╩╚═╝╚═╝  ╩╚═╚═╝═╩╝╩╩╚═╚═╝╚═╝ ╩ ╚═╝  └┘   ═╩╝╚═╝╚╩╝╝╚╝╩═╝╚═╝╩ ╩═╩╝╚═╝
+  '/terms':                   '/legal/terms',
+  '/logout':                  '/api/v1/account/logout',
+
+
+  //  ╦ ╦╔═╗╔╗ ╦ ╦╔═╗╔═╗╦╔═╔═╗
+  //  ║║║║╣ ╠╩╗╠═╣║ ║║ ║╠╩╗╚═╗
+  //  ╚╩╝╚═╝╚═╝╩ ╩╚═╝╚═╝╩ ╩╚═╝
+  // …
+
+
+  //  ╔═╗╔═╗╦  ╔═╗╔╗╔╔╦╗╔═╗╔═╗╦╔╗╔╔╦╗╔═╗
+  //  ╠═╣╠═╝║  ║╣ ║║║ ║║╠═╝║ ║║║║║ ║ ╚═╗
+  //  ╩ ╩╩  ╩  ╚═╝╝╚╝═╩╝╩  ╚═╝╩╝╚╝ ╩ ╚═╝
+  // Note that, in this app, these API endpoints may be accessed using the `Cloud.*()` methods
+  // from the Parasails library, or by using those method names as the `action` in <ajax-form>.
+  '/api/v1/account/logout':                           { action: 'account/logout' },
+  'PUT   /api/v1/account/update-password':            { action: 'account/update-password' },
+  'PUT   /api/v1/account/update-profile':             { action: 'account/update-profile' },
+  'PUT   /api/v1/account/update-billing-card':        { action: 'account/update-billing-card' },
+  'PUT   /api/v1/entrance/login':                        { action: 'entrance/login' },
+  'POST  /api/v1/entrance/signup':                       { action: 'entrance/signup' },
+  'POST  /api/v1/entrance/send-password-recovery-email': { action: 'entrance/send-password-recovery-email' },
+  'POST  /api/v1/entrance/update-password-and-login':    { action: 'entrance/update-password-and-login' },
+  'POST  /api/v1/deliver-contact-form-message':          { action: 'deliver-contact-form-message' },
+
+  'POST /api/follow': {
+    fn: async function(req, res) {
+      const userId = req.param('userId');
+
+      // if (userId === 0) {
+      //   console.log('got userId 0 so waiting');
+      //   await new Promise(resolve => setTimeout(resolve, 5000));
+      //   console.log('dont waiating for userId 0');
+      // } else {
+      //   console.log('got non-userId 0 (' + userId + ') so not waiting');
+      // }
+
+      if (userId === 0) {
+        retryCnt++;
+        if (retryCnt < 5) {
+          return res.status(400).send('not yet hit retry 5');
+        }
+      }
+
+      console.log('Followed userId:' + userId);
+
+      return res.send('Followed userId:' + userId);
+    }
+  }
+
+};
